d50509a079bbecb6510b8e3e06b44dc83fae92e9
AzureAD\김해정(HaleyKim) <haley.kim@biskitlab.com>
2022-06-24 17:57:51 +0900

    서버 - 브라우저 연결 및 메시지 주고받기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,25 @@
-alert('hi')
+//프론트에서 웹소켓 연결 요청 코드
+//프론트에서의 socket은 서버로의 연결을 의미한다.
+const socket = new WebSocket(url = `ws://${window.location.host}`);
+
+
+//연결
+socket.addEventListener('open', () => {
+    console.log('Connected to Server 🙂');
+})
+
+//메시지 받기
+socket.addEventListener('message', (message) => {
+    console.log('New message: ', message.data);
+})
+
+//서버로의 연결이 끊어졌을 때
+socket.addEventListener('close', () => {
+    console.log("Disconnected to Server 💀");
+})
+
+//메시지 보내기
+setTimeout(() => {
+    socket.send("hello from the browser!!");
+}, 10000);
+
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -8,5 +10,22 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);  //wss://localhost:3000, http://localhost:3000
+
+// 아래 코드로 http 서버와 websocket 서버 둘 다 돌릴 수 있다.
+// 두 프로토콜이 같은 포트에 있었으면 좋겠어서 이렇게 만든 것 뿐이지, 굳이 두개를 다 돌리지 않아도 됨.
+const server = http.createServer(app); //http 서버
+const wss = new WebSocket.Server({ server }); //websocket 서버
+
+//서버에서의 socket은 연결된 브라우저를 의미한다.
+wss.on("connection", (socket) => {
+    console.log('Connected to Browser 🙂');
+    socket.on("close", () => console.log("Disconnected from the Browser 💀"));
+    socket.on("message", (message) => {
+        console.log(message.toString());
+    })
+    socket.send("hello");
+});
+
+
+server.listen(3000, handleListen);
